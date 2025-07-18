dd135cc58d38c9a80b6016a1cbf320edef147d62
EndangeredBubs <evan.messmer@gmail.com>
2025-07-17 23:22:06 -0700

    Initial commit

---
+++ src/main/java/net/endangeredbubs/bubsexpansion/BubsExpansion.java
@@ -0,0 +1,16 @@
+package net.endangeredbubs.bubsexpansion;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BubsExpansion implements ModInitializer {
+	public static final String MOD_ID = "bubsexpansion";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/endangeredbubs/bubsexpansion/BubsExpansionClient.java
@@ -0,0 +1,10 @@
+package net.endangeredbubs.bubsexpansion;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BubsExpansionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
